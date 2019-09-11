abaa994a8ef5782aaf1f749df22a0dd4b1aa11b0
mathevet luca <luca.mathevet@epitech.eu>
2019-09-11 14:10:02 +0200

    rm all

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,7 @@
 import 'package:flutter/material.dart';
-import 'homepage.dart';
+import 'tabbar.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(fontFamily: 'MoskNormal400.ttf'),
-        home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.transparent,
-            elevation: 0.0,
-            title: Text(
-                'Wattsmycons',
-                style: TextStyle(color: Color(0xFF120E4C), fontSize: 25),
-              ),
-          ),
-          body: HomePage(),
-        ));
-  }
+void main() {
+  runApp(TabBarDemo());
 }
 
-class HomePage extends StatefulWidget {
-  State createState() => HomePageState();
-}
